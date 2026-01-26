138b406b44d0036fd99ed9e598b38faadb122892
NNAMDI C AMADI <nnamdiamadi1964@gmail.com>
2026-01-26 05:26:10 -0500

    Create 06_attrition_by_gender.sql

---
+++ sql/06_attrition_by_gender.sql
@@ -0,0 +1,14 @@
+-- ATTRITION BY GENDER
+-- Calculates attrition count and attrition rate for each gender
+
+SELECT
+  Gender,
+  COUNT(*) AS total_employees,
+  SUM(attrition_flag) AS attrition_count,
+  ROUND(
+    SUM(attrition_flag) / COUNT(*) * 100,
+    2
+  ) AS attrition_rate_percentage
+FROM `nnamdi-hr-analytics-capstone.hr_analytics.employee_attrition_clean`
+GROUP BY Gender
+ORDER BY attrition_rate_percentage DESC;
