b8cc1523cc9c6b705008b9b38f5dc946f02f0632
NNAMDI C AMADI <nnamdiamadi1964@gmail.com>
2026-01-26 12:54:57 -0500

    Create 04-total-employees.sql

---
+++ sql/04-total-employees.sql
@@ -0,0 +1,7 @@
+-- TOTAL EMPLOYEES
+-- Calculates the total number of employees in the dataset
+
+SELECT 
+    COUNT(*) AS total_employees
+FROM 
+    `nnamdi-hr-analytics-capstone.hr_analytics.employee_attrition`;
